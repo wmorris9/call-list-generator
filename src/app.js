f56164f43565d97390c3aad38e34216b14d18ce0
Weylin Morris <wm96n2c@gmail.com>
2019-11-15 00:15:04 -0700

    Heroku Compatable

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const path = require('path')
 const listGenerator = require('./utils/phone-lookup')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -28,6 +29,6 @@ app.get('/list', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
 })
